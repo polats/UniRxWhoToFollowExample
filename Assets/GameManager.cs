b9b3e3c8f7ca652d652b7c871ed3bac27011a12e
Paul Gadi <paulgadi@gmail.com>
2017-01-08 18:12:44 +0800

    moved observableWWW into responseStream

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,11 +8,19 @@ public class GameManager : MonoBehaviour {
 	void Start () {
         var requestStream = UniRx.Observable.Return<string>("https://api.github.com/users");
 
-        requestStream.Subscribe(t =>
+        requestStream.Subscribe( t =>
             {
-                ObservableWWW.Get(t).Subscribe(
-                    x => Debug.Log("onSuccess: " + x), // onSuccess
-                    ex => Debug.LogException(ex)); // onError
+                var responseStream = ObservableWWW.Get(t);
+
+                responseStream.Subscribe(
+                    response => // onSuccess
+                    {
+                        Debug.Log("response: " + response); 
+                    },
+                    e => // onError
+                    {
+                        Debug.LogException(e);
+                    });
             });
 	}
 	
