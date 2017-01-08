1e0e392d2ba5cf8d35d7ed786e159749ffbac415
Paul Gadi <paulgadi@gmail.com>
2017-01-08 16:49:56 +0800

    subscribe to requestStream with an ObservableWWW to call the request operation

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,6 +7,13 @@ public class GameManager : MonoBehaviour {
 
 	void Start () {
         var requestStream = UniRx.Observable.Return<string>("https://api.github.com/users");
+
+        requestStream.Subscribe(t =>
+            {
+                ObservableWWW.Get(t).Subscribe(
+                    x => Debug.Log("onSuccess: " + x), // onSuccess
+                    ex => Debug.LogException(ex)); // onError
+            });
 	}
 	
 }
