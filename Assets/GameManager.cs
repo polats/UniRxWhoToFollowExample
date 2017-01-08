f88d208fb0e5f31e1b16baa000751987a6356312
Paul Gadi <paulgadi@gmail.com>
2017-01-08 22:59:26 +0800

    *extra step* convert observableWWW jsonString into list of users

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using SimpleJSON;
 
 public class GameManager : MonoBehaviour {
 
     // set from editor
     public GameObject refreshButton;
+    public GameObject suggestion1Text;
+
+    const string HTML_URL = "html_url";
+    const string LOGIN = "login";
+    const string AVATAR_URL = "avatar_url";
 
 	void Start () {
         
@@ -24,25 +30,54 @@ public class GameManager : MonoBehaviour {
         var responseStream = requestStream.SelectMany(
             requestUrl =>
             {
-                return ObservableWWW.Get(requestUrl);
+                return ObservableWWW.Get(requestUrl).Select(
+                    jsonString =>
+                    {
+                        // convert jsonString to list of users as Dictionaries
+                        JSONNode root = SimpleJSON.JSON.Parse(jsonString);
+                        List<Dictionary<string, string>> listUsers = new List<Dictionary<string, string>>();
+
+                        for (int i = 0; i < root.Count; i++)
+                        {
+                            Dictionary<string, string> user = new Dictionary<string, string>();
+                            user[HTML_URL] = root[i][HTML_URL];
+                            user[LOGIN] = root[i][LOGIN];
+                            user[AVATAR_URL] = root[i][AVATAR_URL];
+                            listUsers.Add(user);
+                        }
+                            
+                        return listUsers;
+                    });
             });
 
         responseStream.Subscribe(
             response => // onSuccess
             {
-                Debug.Log("response: " + response); 
+                // response is a list of users
+                foreach (Dictionary<string,string> user in response)
+                {
+                    Debug.Log("user: " + user[LOGIN]); 
+                }
             },
             e => // onError
             {
                 Debug.LogException(e);
             });
                 
-
+        /*
         var suggestion1Stream = responseStream.Select(
             listUsers =>
             {
-                return listUsers[Random.Range(0, listUsers.Length)];
+                return listUsers.ToString();
             });
+
+        suggestion1Stream.Subscribe(
+            suggestion =>
+            {
+                suggestion1Text.GetComponentInChildren<Text>().text = suggestion;
+            });
+            */
 	}
+    
 	
 }
