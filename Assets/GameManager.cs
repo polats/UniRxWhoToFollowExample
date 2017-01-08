d36487797094fa5a7ca8d1e0caa0bb97766a05e7
Paul Gadi <paulgadi@gmail.com>
2017-01-08 19:06:42 +0800

    remove intermediate streams

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,18 +13,13 @@ public class GameManager : MonoBehaviour {
         
         var refreshClickStream = refreshButton.GetComponent<Button>().onClick.AsObservable();
 
-        var requestOnRefreshStream = refreshClickStream.Select(
+        var requestStream = refreshClickStream.Select(
             t =>
             {
                 var randomOffset = Random.Range(1,500);
                 return "https://api.github.com/users?since=" + randomOffset;
-            });
-
-        var startupRequestStream = UniRx.Observable.Return<string>("https://api.github.com/users");
-
-        var requestStream = UniRx.Observable.Merge(
-            requestOnRefreshStream, startupRequestStream
-            );
+            })
+            .Merge(UniRx.Observable.Return<string>("https://api.github.com/users"));
         
         var responseStream = requestStream.SelectMany(
             requestUrl =>
