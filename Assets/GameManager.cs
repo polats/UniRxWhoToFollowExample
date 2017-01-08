cd4653f7ef6f4c6ad4d2a4aac6b04e1fea4db9d2
Paul Gadi <paulgadi@gmail.com>
2017-01-08 14:27:28 +0800

    create requestStream with a Return operation containing the api URL

---
+++ Assets/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class GameManager : MonoBehaviour {
+
+	void Start () {
+        var requestStream = UniRx.Observable.Return<string>("https://api.github.com/users");
+	}
+	
+}
